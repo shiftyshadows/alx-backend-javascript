b321c2dde9071fb9c0dfd7cb6059699e1b312bb4
ShiftyShadows <purple.quasar@proton.me>
2024-10-29 15:32:24 +0300

    1. Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,6 @@
-export default function taskBlock(trueOrFalse) {
-  if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
-    return [task, task2];
-  }
-
+export default function taskBlock() {
   const task = false;
   const task2 = true;
+
   return [task, task2];
 }
