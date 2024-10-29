ac6bf695397ce1ed2a3f96414ac47f3ed08bff67
ShiftyShadows <purple.quasar@proton.me>
2024-10-29 16:46:25 +0300

    10. For...of Loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,3 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  return array.map((value) => appendString + value);
+  const result = [];
+  for (const value of array) {
+    result.push(appendString + value);
+  }
+
+  return result;
 }
