ca28179ba838e22feeae7420e80a5a762574b380
ShiftyShadows <purple.quasar@proton.me>
2024-11-05 14:25:08 +0300

    6. Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -7,6 +7,8 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     uploadPhoto(fileName),
   ]).then((results) => results.map((result) => ({
     status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
+    value: result.status === 'fulfilled'
+      ? result.value
+      : String(result.reason), // Convert Error object to string
   })));
 }
