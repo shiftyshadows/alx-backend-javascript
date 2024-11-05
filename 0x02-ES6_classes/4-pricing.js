3e99f7457e6f52b7d07ab3684614d49a7072cd00
ShiftyShadows <purple.quasar@proton.me>
2024-11-05 16:47:50 +0300

    4. Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,56 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    // Validate types of the constructor parameters
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of the Currency class');
+    }
+
+    /* eslint-disable no-underscore-dangle */
+    // Set private attributes
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getter and setter for `amount`
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  // Getter and setter for `currency`
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of the Currency class');
+    }
+    this._currency = value;
+  }
+
+  // Method to display full price
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  /* eslint-disable no-underscore-dangle */
+  // Static method to convert price
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Both amount and conversionRate must be numbers');
+    }
+    return amount * conversionRate;
+  }
+}
