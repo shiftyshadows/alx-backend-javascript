b324a89de3a114b23707d01033ffe9875f3e0078
ShiftyShadows <purple.quasar@proton.me>
2024-11-05 16:53:54 +0300

    6. Inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,27 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    // Call the parent class constructor with sqft
+    super(sqft);
+
+    // Validate floors type
+    if (typeof floors !== 'number') {
+      throw new TypeError('floors must be a number');
+    }
+    /* eslint-disable no-underscore-dangle */
+    // Set private attribute
+    this._floors = floors;
+  }
+
+  // Getter for floors
+  get floors() {
+    return this._floors;
+  }
+
+  // Override evacuationWarningMessage method
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
+  /* eslint-disable no-underscore-dangle */
+}
