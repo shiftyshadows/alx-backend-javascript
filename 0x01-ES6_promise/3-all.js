ccfc68d4a368b2057a96df1a0fb93c230bf51761
ShiftyShadows <purple.quasar@proton.me>
2024-11-05 16:11:29 +0300

    3. Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-    return Promise.all([uploadPhoto(), createUser()])
-        .then(([photoResponse, userResponse]) => {
-            console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
-        })
-        .catch(() => {
-            console.log('Signup system offline');
-        });
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
 
 export default handleProfileSignup;
-
