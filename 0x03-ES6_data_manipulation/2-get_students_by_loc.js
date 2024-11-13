330a672882c9f2e87880b9c242489587cba38443
ShiftyShadows <purple.quasar@proton.me>
2024-11-13 17:33:21 +0300

    2. Filter

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,6 @@
+export default function getStudentsByLocation(students, city) {
+  if (!Array.isArray(students) || typeof city !== 'string') {
+    return [];
+  }
+  return students.filter((student) => student.location === city);
+}
